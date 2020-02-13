6633b175296c386fd3992849e30b24e5d0a307f3
Kim Jioo <37768795+jwkim101201@users.noreply.github.com>
2020-02-13 14:51:23 +0900

    permission

--- src/ksensial/commands/CustomItemCommand.java
+++ src/ksensial/commands/CustomItemCommand.java
@@ -19,66 +19,68 @@ public class CustomItemCommand implements CommandExecutor {
             return true;
         } else {
             Player p = (Player) sender;
-            if (args.length >= 1) {
-                switch (args[0]) {
-                    case "enchant": {
-                        if (Enchantment.getByName(args[1]) == null) {
-                            p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant> <인챈트 레벨>");
-                        } else {
-                            if (!(p.getInventory().getItemInMainHand() == null)) {
-                                if (args.length == 3) {
-                                    try {
-                                        p.getInventory().getItemInMainHand().addEnchantment(Enchantment.getByName(args[1]), NumberUtils.toInt(args[2]));
-                                    } catch (IllegalArgumentException ignored) {
-                                        p.sendMessage("§c인챈트 대상이 잘못되었거나 인챈트 최대 레벨을 넘어섰습니다");
-                                    } catch (IndexOutOfBoundsException ignored) {
-                                        p.sendMessage("§c올바른 사용법: /customitem enchant <인챈트 이름> <인챈트 레벨>");
+            if (p.hasPermission("ksensial.customitem")) {
+                if (args.length >= 1) {
+                    switch (args[0]) {
+                        case "enchant": {
+                            if (Enchantment.getByName(args[1]) == null) {
+                                p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant> <인챈트 레벨>");
+                            } else {
+                                if (!(p.getInventory().getItemInMainHand() == null)) {
+                                    if (args.length == 3) {
+                                        try {
+                                            p.getInventory().getItemInMainHand().addEnchantment(Enchantment.getByName(args[1]), NumberUtils.toInt(args[2]));
+                                        } catch (IllegalArgumentException ignored) {
+                                            p.sendMessage("§c인챈트 대상이 잘못되었거나 인챈트 최대 레벨을 넘어섰습니다");
+                                        } catch (IndexOutOfBoundsException ignored) {
+                                            p.sendMessage("§c올바른 사용법: /customitem enchant <인챈트 이름> <인챈트 레벨>");
+                                        }
                                     }
+                                } else {
+                                    p.sendMessage("§c인챈트할 아이템을 손에 들어야 합니다");
+                                }
+                            }
+                            break;
+                        }
+                        case "name": {
+                            if (!(p.getInventory().getItemInMainHand() == null)) {
+                                ItemMeta im = p.getInventory().getItemInMainHand().getItemMeta();
+                                if (args.length == 2) {
+                                    im.setDisplayName(args[1]);
+                                    p.getInventory().getItemInMainHand().setItemMeta(im);
+                                } else {
+                                    p.sendMessage("§c올바른 사용법: /customitem name <변경할 이름>");
                                 }
                             } else {
-                                p.sendMessage("§c인챈트할 아이템을 손에 들어야 합니다");
+                                p.sendMessage("§c이름을 바꿀 아이템을 손에 들어야 합니다");
                             }
+                            break;
                         }
-                        break;
-                    }
-                    case "name": {
-                        if (!(p.getInventory().getItemInMainHand() == null)) {
-                            ItemMeta im = p.getInventory().getItemInMainHand().getItemMeta();
-                            if (args.length == 2) {
-                                im.setDisplayName(args[1]);
-                                p.getInventory().getItemInMainHand().setItemMeta(im);
+                        case "description": {
+                            if (!(p.getInventory().getItemInMainHand() == null)) {
+                                if (args.length == 2) {
+                                    List<String> loreList = new ArrayList<String>();
+                                    ItemMeta im = p.getInventory().getItemInMainHand().getItemMeta();
+                                    loreList.add(args[1]);
+                                    im.setLore(loreList);
+                                    p.getInventory().getItemInMainHand().setItemMeta(im);
+                                } else {
+                                    p.sendMessage("§c올바른 사용법: /customitem description <변경할 설명>");
+                                }
                             } else {
-                                p.sendMessage("§c올바른 사용법: /customitem name <변경할 이름>");
+                                p.sendMessage("§c설명을 바꿀 아이템을 손에 들어야 합니다");
                             }
-                        } else {
-                            p.sendMessage("§c이름을 바꿀 아이템을 손에 들어야 합니다");
+                            break;
                         }
-                        break;
-                    }
-                    case "description": {
-                        if (!(p.getInventory().getItemInMainHand() == null)) {
-                            if (args.length == 2) {
-                                List<String> loreList = new ArrayList<String>();
-                                ItemMeta im = p.getInventory().getItemInMainHand().getItemMeta();
-                                loreList.add(args[1]);
-                                im.setLore(loreList);
-                                p.getInventory().getItemInMainHand().setItemMeta(im);
-                            } else {
-                                p.sendMessage("§c올바른 사용법: /customitem description <변경할 설명>");
-                            }
-                        } else {
-                            p.sendMessage("§c설명을 변경할 아이템을 손에 들어야 합니다");
+                        default: {
+                            p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant>");
                         }
-                        break;
                     }
-                    default: {
-                        p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant>");
-                    }
+                } else {
+                    p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant>");
                 }
-            } else {
-                p.sendMessage("§c올바른 사용법: /customitem <바꿀거(name/description/enchant>");
             }
+            return true;
         }
-        return true;
     }
 }
